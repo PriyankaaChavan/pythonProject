1b2fb408604f70c12848d88142643dbd868cf70d
PriyankaaChavan <priyanka1739@gmail.com>
2024-08-21 14:19:17 +0530

    Lab005 modification

--- src/ex_10082024/Lab004.py
+++ src/ex_10082024/Lab004.py
@@ -1,7 +1,20 @@
-#WAP to find max number between 2 integers.
-#ex.  34,101
-
-print(max(34,101,515,1001,-1,81.3))
-print(max(34,101,515,1001,-1,81.3, "PC")) #will get an error because it will give an error, str and int cant not compare.
+# 1+1=2- syntax error
+# Source code- code written by human.for that we have to follow some rules.
+# which are python rules.
 
 
+# WAP to find max number between 2 integers.
+# ex.  34,101
+from sys import setdlopenflags
+
+# print(max(34,101,515,1001,-1,81.3))
+# print(max(34,101,515,1001,-1,81.3, "PC")) #will get an error because it will give an error, str and int cant not compare.
+# age=30
+# name="pc"
+# value=3.14
+# complex_num= 1+2j
+# print(type(age))
+# print(type(name))
+# print(type(value))
+# print(type(complex_num.real))
+# print(type(complex_num.imag))
--- src/ex_10082024/Lab005.py
+++ src/ex_10082024/Lab005.py
@@ -1,1 +1,53 @@
-#Keywords and Identifiers In Python
+# # Keywords and Identifiers In Python
+# # Keywords also known as reserved words
+# # Can not use as variable name
+# # variable-
+# # import keyword
+# #
+# # print(keyword.kwlist)
+#
+# # ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await',
+# # 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',
+# # 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda'
+# # , 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+# # Can not use as variable name
+# # variable- it is used to store the value- container type.
+# # ex. age=30
+#
+# # Dynamically Type-means at the run time, data of the variable can change
+# # and we dont need to mentions the data type.
+#
+# # int a= 10 - JAVA syntax where need to specify but in python
+# # a=10 -python will change as per valus. ex ->
+# # age=40
+# # print(type(age)) #int
+# #
+# # print("PC")
+# #
+# # age="PC"
+# # print(type(age)) #str
+# # print(type(age)) #str
+# # print(type(age)) #str
+#
+#
+# # o/p=
+# # <class 'int'>
+# # PC
+# # <class 'str'>
+#
+# a = 10
+# b = 10
+# c = 10
+#
+# sum = (a + b + c )
+# #print(sum)
+#
+# sum = sum + 1
+# #pprint(sum)
+#
+# #sum+1 #no changes because we are just adding value but not storing (=) anywhere.
+# print(sum)
+#
+
+a,b,c=10,20,30
+print(a,b,c,sep="_")
